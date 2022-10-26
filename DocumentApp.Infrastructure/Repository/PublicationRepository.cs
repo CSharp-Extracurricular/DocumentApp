382c767af678ca44f7d68919f16476fb7dfea03e
Cucmbr <vanvanich531@gmail.com>
2022-10-26 19:46:15 +0300

    ConnectionStr + Repo
    LocalDb connection string
    Repository folder with async add fun

---
+++ DocumentApp.Infrastructure/Repository/PublicationRepository.cs
@@ -0,0 +1,24 @@
+﻿using DocumentApp.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DocumentApp.Infrastructure.Repository
+{
+    public class PublicationRepository
+    {
+        private readonly Context _context;
+        public Context UnitOfWork
+        {
+            get
+            {
+                return _context;
+            }
+        }
+
+        public async Task AddAsync(Publication publication)
+        {
+            _context.Publications.Add(publication);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
