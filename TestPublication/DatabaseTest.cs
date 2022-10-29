2156fac5150a7ab231891b7227cd5a0042153205
waffencode <waffencode@gmail.com>
2022-10-29 20:14:33 +0300

    Подготовлен класс для тестирования взаимодействия с базой данных

---
+++ TestPublication/DatabaseTest.cs
@@ -0,0 +1,17 @@
+namespace TestPublication
+{
+    public class DatabaseTest
+    {
+        [Fact]
+        public void TestPublicationAddition()
+        {
+
+        }
+
+        [Fact]
+        public void TestPublicationDeletion()
+        {
+
+        }
+    }
+}
