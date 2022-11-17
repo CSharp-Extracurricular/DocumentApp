920b50d9f8c1d0c16495795b7007c4ffbc414ec9
Nika <92479422+nikyn4ik@users.noreply.github.com>
2022-11-18 02:24:13 +0300

    Create controller.cs

---
+++ DocumentApp.API/Controllers/Controller.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DocumentApp.Infrastructure;
+using DocumentApp.Domain;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DocumentApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Controller : ControllerBase
+    {
+        private readonly Context _context;
+        private readonly PublicationRepository _publicationRepository;
+        public Controller(Context context)
+        {
+            _context = context;
+            _publicationRepository = new PublicationRepository(_context);
+        }
+
+        // GET: api/Publication
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Publication>>> GetPublications()
+        {
+            //return await _context.Publications.ToListAsync();
+            return await _publicationRepository.GetAllAsync();
+        }
+
+        // GET: api/Publication/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Publication>> GetPublication(int id)
+        {
+            var publication = await _context.Publications.FindAsync(id);
+            //var publication = await _publicationRepository.GetByIdAsync(id);
+            if (publication == null)
+            {
+                return NotFound();
+            }
+            return publication;
+        }
+
+        // PUT: api/Publication/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPublication(int id, Publication publication)
+        {
+            if (id != publication.Id)
+            {
+                return BadRequest();
+            }
+            await _publicationRepository.UpdateAsync(publication);
+
+            return NoContent();
+        }
+
+        // POST: api/Publication
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Publication>> PostPublications(Publication publication)
+        {
+            await _publicationRepository.AddAsync(publication);
+            return CreatedAtAction("GetPublications", new { id = publication.Id }, publication);
+        }
+
+        // DELETE: api/Publication/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePublication(long id)
+        {
+            var publication = await _publicationRepository.GetByIdAsync(id);
+            if (publication == null)
+            {
+                return NotFound();
+            }
+
+            _context.Publications.Remove(publication);
+            await _context.SaveChangesAsync();
+            //await _publicationRepository.DeleteAsync(id);
+
+            return NoContent();
+        }
+
+        //private bool PublicationExists(int id)
+        //{
+        //    return _context.Publications.Any(e => e.Id == id);
+        //}
+    }
+}
