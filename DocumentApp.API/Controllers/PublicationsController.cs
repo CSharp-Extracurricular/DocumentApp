836c4fdd4ccd236f8261afc7276f3bde9fa71516
waffencode <waffencode@gmail.com>
2022-11-18 17:47:26 +0300

    Изменён класс контроллера публикаций

--- DocumentApp.API/Controllers/PublicationsController.cs
+++ DocumentApp.API/Controllers/PublicationsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using DocumentApp.Infrastructure;
+using DocumentApp.Domain;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +10,78 @@ namespace DocumentApp.API.Controllers
     [ApiController]
     public class PublicationsController : ControllerBase
     {
-        // GET: api/<PublicationsController>
+        private readonly Context _context;
+        private readonly PublicationRepository _publicationRepository;
+        public PublicationsController(Context context)
+        {
+            _context = context;
+            _publicationRepository = new PublicationRepository(_context);
+        }
+
+        // GET: api/Publication
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<Publication>>> GetPublications()
         {
-            return new string[] { "value1", "value2" };
+            //return await _context.Publications.ToListAsync();
+            return await _publicationRepository.GetAllAsync();
         }
 
-        // GET api/<PublicationsController>/5
+        // GET: api/Publication/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<Publication>> GetPublication(Guid id)
         {
-            return "value";
+            //var publication = await _context.Publications.FindAsync(id);
+            var publication = await _publicationRepository.GetByIdAsync(id);
+            if (publication == null)
+            {
+                return NotFound();
+            }
+            return publication;
         }
 
-        // POST api/<PublicationsController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<PublicationsController>/5
+        // PUT: api/Publication/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> PutPublication(Guid id, Publication publication)
         {
+            if (id != publication.Id)
+            {
+                return BadRequest();
+            }
+            await _publicationRepository.UpdateAsync(publication);
+
+            return NoContent();
         }
 
-        // DELETE api/<PublicationsController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // POST: api/Publication
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Publication>> PostPublications(Publication publication)
         {
+            await _publicationRepository.AddAsync(publication);
+            return CreatedAtAction("GetPublications", new { id = publication.Id }, publication);
+        }
+
+        // DELETE: api/Publication/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePublication(Guid id)
+        {
+            var publication = await _publicationRepository.GetByIdAsync(id);
+            if (publication == null)
+            {
+                return NotFound();
+            }
+
+            _context.Publications.Remove(publication);
+            await _context.SaveChangesAsync();
+            //await _publicationRepository.DeleteAsync(id);
+
+            return NoContent();
+        }
+
+        private bool PublicationExists(Guid id)
+        {
+            return _context.Publications.Any(e => e.Id == id);
         }
     }
 }
