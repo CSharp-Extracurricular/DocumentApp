745c3240baa5d5fa79f687d570bfff3dff7f70c3
waffencode <waffencode@gmail.com>
2023-03-28 17:29:04 +0300

    Add mail service infrastructure

---
+++ DocumentApp.API/Controllers/MailController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        private readonly Mailer _mailer = new("", "", true, 587);
+
+        [HttpPost]
+        public void Send(MailAddress receiverAddress)
+        {
+
+        }
+    }
+}
